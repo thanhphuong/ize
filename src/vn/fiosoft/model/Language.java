3d53e4c6d90536418a7ddc5111bc8921a3299a8b
Bui Thanh Phuong <btphuong@acrobits.cz>
2012-11-12 23:47:53 +0700

    model sentence

---
+++ src/vn/fiosoft/model/Language.java
@@ -0,0 +1,6 @@
+package vn.fiosoft.model;
+
+public class Language {
+	public static final String ENGLISH = "en";
+	public static final String VIETNAM = "vi";
+}
---
+++ src/vn/fiosoft/model/Sentence.java
@@ -0,0 +1,49 @@
+package vn.fiosoft.model;
+
+public class Sentence {
+
+	private int id;
+	private String content;
+	private Language language;
+	private int group;
+
+	public Sentence() {
+		this.id = 0;
+		this.content = "";
+		this.language = null;
+		this.group = 0;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Language getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(Language language) {
+		this.language = language;
+	}
+
+	public int getGroup() {
+		return group;
+	}
+
+	public void setGroup(int group) {
+		this.group = group;
+	}
+
+}
